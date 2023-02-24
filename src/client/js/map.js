3122e57e26fba13655b2c7fccbbae0a32f81d8af
ohhyei <ohhyeiohhyeiohhyei@gmail.com>
2023-02-24 14:41:14 +0900

    ver.1.0.3

--- src/client/js/map.js
+++ src/client/js/map.js
@@ -6,7 +6,6 @@ let all = [],
 // 지도를 담을 영역의 DOM 레퍼런스
 const mapContainer = document.getElementById('map');
 
-
 // 지도를 생성할 때 필요한 기본 옵션
 const mapOptions = {
     level: 9, //지도의 레벨(확대, 축소 정도)
@@ -52,42 +51,48 @@ function displayMarker(locPosition) {
     map.setCenter(locPosition);      
 }
 
-var marker = [];
-// 지도에 마커를 표시합니다
-
-// 커스텀 오버레이에 표시할 컨텐츠 입니다
-function makeHtml(data) {
-    var mapBox = 
-            '<div class="mapWrap">' + 
-            '   <div class="mapInfo">' + 
-            '       <div class="mapTitle">' + 
-                        data.name +
-            '           <div class="mapClose" onclick="closeOverlay()" title="닫기"></div>' + 
-            '       </div>' + 
-            '       <div class="mapContents">' + 
-            '           <div class="mapDesc">' + 
-            `                <div class="mapEllipsis">${data.address}</div>` +
-            '           </div>' + 
-            '       </div>' + 
-            '   </div>' +    
-            '</div>';
-    return mapBox
-}
-
 // fetch() 함수로 DB의 데이터 불러오기
 const mapFetch = async () => {
-    const data = await(await fetch("/map/info")).json();
+    var data = await(await fetch("/map/info")).json();
     console.log(data)
+
     for(i=0; i < data.length; i++) {
         // 마커가 표시될 위치
         let markerPosition = new kakao.maps.LatLng(data[i].latitude, data[i].longitude);
+
+        // 이미지 주소
+        let imgSrc = "/file/img/mappin.png";
+        // 이미지 크기
+        let imgSize = new kakao.maps.Size(22, 32);
+        // 마커 이미지
+        let markerImage = new kakao.maps.MarkerImage(imgSrc, imgSize);
 
         // 마커 생성
         var marker = new kakao.maps.Marker({
             map: map, 
             position: markerPosition,
+            image: markerImage
         });
     }
 
+    // 카테고리 눌렀을 때 메뉴 스타일 변경
+    const menuStyleChange = (clickDom) => {
+        for(let i=0; i < mapCategory.length; i++) {
+            if(mapCategory[i] === clickDom) mapCategory[i].classList.add("mapCategoryOn")
+            else mapCategory[i].classList.remove("mapCategoryOn")
+        }
+    }
+
+    // 카테고리 눌렀을 때 실행할 함수
+    const clickMenu = (e) => {
+        const clickDom = e.currentTarget;
+        if(clickDom.classList.contains("mapCategoryOn")) return;
+        menuStyleChange(clickDom);
+    }
+
+    // 카테고리 클릭 이벤트
+    for(let i=0; i < mapCategory.length; i++) {
+        mapCategory[i].addEventListener("click", clickMenu);
+    }
 }
 mapFetch();
